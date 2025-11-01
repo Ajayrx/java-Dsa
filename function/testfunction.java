c391cd8d737e43161c7cd7034ad3ae84fa9a0ead
Ajayrx <ajay0i0know@gmail.com>
2025-11-01 23:55:17 +0530

    name fix test - testfunction

--- function/testfunction.java
+++ function/testfunction.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class test {
+public class testfunction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number to get its factorial: ");
